d96ecdddb090448a023a1701b4de3576c6d7d405
sharundas <123480220+sharundas@users.noreply.github.com>
2023-09-30 20:44:35 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,6 +17,12 @@ app.listen(3000,() => {
   console.log('Server is running on port 3000')
 });
 
+
+app.use("/api/user", userRouter);
+
+// app.get('/test' , (req,res) => {
+//   res.send('Hello World');
+// })
 
 // sharun  pass - rFqRpvW3eyFVjOKh
 
