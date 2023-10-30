da8905126b6a18816a4b9422734f8222f1ab9b91
sharundas <123480220+sharundas@users.noreply.github.com>
2023-10-30 09:33:01 +0530

    delete functionality

--- api/controllers/user.ontroller.js
+++ api/controllers/user.ontroller.js
@@ -23,9 +23,20 @@ export const updateUser = async (req, res , next) => {
     }
   }, {new:true})
  const {password, ...rest} = updatedUser._doc
- req.status(200).json(rest)
+ res.status(200).json(rest)
 
  } catch (error) {
   next(error);
  }
+}
+
+export const deleteUser = async(req,res,next) => {
+  if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account'));
+  try {
+   await User.findByIdAndDelete(req.params.id);
+   res.clearCookies('access_token');
+   res.status(200).json('User has been delected');
+  } catch (error) {
+    next(error);
+  }
 }
